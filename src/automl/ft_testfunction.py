375418f07663c392b85885a69b554ecb5a022e86
Meganton <ameganton@gmail.com>
2024-07-10 17:45:25 +0200

    added test-function

---
+++ src/automl/ft_testfunction.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def global_1D(x):
+    return (1 / (1 + np.exp(-(0.1*x**3 - 0.5*x**2 + 0.2*x )))).flatten()
+
+def global_xD(x):
+    if x.shape[0] == 1:
+        return global_1D(x)
+    else:
+        raise NotImplementedError("Only 1D data is supported")
+
+def local_1D(x,f):
+    return (global_1D(x)+(1-global_1D(x))*np.exp(-0.1*f)) + np.random.normal(0,0.01,f.shape)#*0.5*np.sqrt(f)
+
+def local_xD(x,f):
+    if x.shape[0] == 1:
+        return local_1D(x,f)
+    else:
+        raise NotImplementedError("Only 1D data is supported")
