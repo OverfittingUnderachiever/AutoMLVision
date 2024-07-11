e9191773025764142a4ba746de012ea383d9bf64
Meganton <ameganton@gmail.com>
2024-07-11 04:45:12 +0200

    added equations 19-21

--- src/automl/ft_testfunction.py
+++ src/automl/ft_testfunction.py
@@ -9,11 +9,11 @@ def global_xD(x):
     else:
         raise NotImplementedError("Only 1D data is supported")
 
-def local_1D(x,f):
-    return (global_1D(x)+(1-global_1D(x))*np.exp(-0.1*f)) + np.random.normal(0,0.01,f.shape)#*0.5*np.sqrt(f)
+def local_1D(x,f,noise=0.1):
+    return np.round((global_1D(x)+(1-global_1D(x))*np.exp(-0.1*f)) + np.random.normal(0,noise,f.shape),2)#*0.5*np.sqrt(f)
 
-def local_xD(x,f):
+def local_xD(x,f,noise=0.1):
     if x.shape[0] == 1:
-        return local_1D(x,f)
+        return local_1D(x,f,noise)
     else:
         raise NotImplementedError("Only 1D data is supported")
